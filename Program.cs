2c14fc91667f357d494a23fae61c6465b400b6b3
Expertz <xeniapliner@gmail.com>
2023-07-02 01:22:08 +0300

    Task 01

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число (>0)
+//и выводит все числа от 1 до этого числа 
+
+Console.WriteLine("Put a number    ");
+int Aa = int.Parse(Console.ReadLine());
+int Bb = 1;
+
+while (Bb <= Aa)
+{
+ Console.Write($"{Bb} ");
+ Bb = Bb + 1;
+}
